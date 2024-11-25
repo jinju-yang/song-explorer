e60744f500344882dacd5b2ba2ea1ec7568c249d
jinju-yang <jinjuyang@sookmyung.ac.kr>
2024-11-25 21:57:31 +0900

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import SearchPage from "./pages/SearchPage";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LikedPage from "./pages/LikedPage";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <SearchPage />
-      </div>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<SearchPage />} />
+            <Route path="/liked" element={<LikedPage />} />
+          </Routes>
+        </div>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 }
 
--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import SearchBar from "../../components/SearchBar";
 import SongList from "../../components/SongList";
+import { useNavigate } from "react-router-dom";
 
 const SearchPage = () => {
+  const navigate = useNavigate();
   return (
     <div>
+      <button
+        onClick={() => {
+          navigate("/liked");
+        }}
+      >
+        Liked
+      </button>
       <SearchBar />
       <SongList />
     </div>
